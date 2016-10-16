28beaf9dea8005ae7f780228a191368b7fd296e7
Trung Ly <trungly135@yahoo.com>
2016-10-16 00:15:54 -0700

    Boilerplate for activities up

---
+++ app/src/main/java/dubhacks/netfridge/RecipeSearchActivity.java
@@ -0,0 +1,13 @@
+package dubhacks.netfridge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_search);
+    }
+}
